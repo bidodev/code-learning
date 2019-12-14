1e6511f2ffda81a271cb21ed1262cbc18e744a3f
Claudinei Bido <bido.c@live.com>
2019-12-14 18:18:31 +0100

    Changes to be committed: 	modified:   javascript/script.js 	Numbers is Javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,19 @@
+/*
 var age = prompt('What is your age?');
 document.getElementById('someText').innerHTML = age;
+*/
+
+//numbers in Javascript
+//Increment number by 1
+var number = 10;
+number++;
+
+//decrement number by 1
+number--;
+
+//Divide, multiiply *, remainder %
+console.log(number / 6);
+
+//Increment / decrement by any number you want.
+number += 10;
+console.log(number);
