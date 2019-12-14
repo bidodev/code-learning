69af4ec5af3d380fe1fd55d0888cc2d0b0beeb78
Claudinei Bido <bido.c@live.com>
2019-12-14 18:36:20 +0100

    Changes to be committed: 	modified:   javascript/index.html 	modified:   javascript/script.js 	Javascript Basic Functions

--- javascript/script.js
+++ javascript/script.js
@@ -17,3 +17,32 @@ console.log(number / 6);
 //Increment / decrement by any number you want.
 number += 10;
 console.log(number);
+
+/* Functions
+1. Create a function
+2. Call the function
+*/
+
+function fun(){
+    console.log('This is a function');
+}
+
+//call
+fun();
+
+/* Let's create a function that takes in a name and says hello followed by your name 
+
+For example
+Name: "Bido"
+Return: "Hello Bido"
+
+*/
+
+function greeting (yourName){
+    
+    var result = (`Hello ${yourName}`);
+    document.getElementById ('myName').innerHTML = result;
+
+}
+var name = prompt ('What is your name?');
+greeting(name);
