4a32c2d6b210459dcec54d17342585edfb874bb5
Claudinei Bido <bido.c@live.com>
2020-02-09 20:01:17 +0100

    Committed with Git-Upload

--- javascript/challenges/romanconverter/solution.js
+++ javascript/challenges/romanconverter/solution.js
@@ -4,17 +4,38 @@
  *
  * All roman numerals answers should be provided in upper-case.
  */
-const ROMAN_NUMBERS = [
-  { romanNumber: "I", from: 1 }, //0
-  { romanNumber: "V", from: 5 } //1
+
+const ROMAN_DECIMAL_VALUES = [
+  { d: 1, r: "I" },
+  { d: 4, r: "IV" },
+  { d: 5, r: "V" },
+  { d: 9, r: "IX" },
+  { d: 10, r: "X" },
+  { d: 40, r: "XL" },
+  { d: 50, r: "L" },
+  { d: 90, r: "XC" },
+  { d: 100, r: "C" },
+  { d: 400, r: "CD" },
+  { d: 500, r: "D" },
+  { d: 900, r: "CM" },
+  { d: 1000, r: "M" }
 ];
 
 const convertToRoman = num => {
-  //   if (num == ROMAN_NUMBERS[0].from) {
-  //     console.log(ROMAN_NUMBERS[0].romanNumber);
-  //   }
+  let saveString = num.split("");
+  console.log("DEBUG: saveString", saveString);
+
+  for (let i = 1; i < saveString.length; i++) {
+    // restNumber += "0";
+  }
+  //   number = saveString.shift() + restNumber;
+  //   console.log(number);
 };
 
-convertToRoman(1);
+convertToRoman("84");
 
 //Under development
+
+/**
+ *
+ */
