9c6339f378c019976952fc1dc3cfb296d49bbad1
Claudinei Bido <bido.c@live.com>
2020-02-09 19:10:09 +0100

    challenge started

---
+++ javascript/challenges/romanconverter/solution.js
@@ -0,0 +1,20 @@
+/**
+ * JavaScript Algorithms and Data Structures Projects: Roman Numeral Converter
+ * Convert the given number into a roman numeral.
+ *
+ * All roman numerals answers should be provided in upper-case.
+ */
+const ROMAN_NUMBERS = [
+  { romanNumber: "I", from: 1 }, //0
+  { romanNumber: "V", from: 5 } //1
+];
+
+const convertToRoman = num => {
+  //   if (num == ROMAN_NUMBERS[0].from) {
+  //     console.log(ROMAN_NUMBERS[0].romanNumber);
+  //   }
+};
+
+convertToRoman(1);
+
+//Under development
