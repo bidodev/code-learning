b93000c3ea59501600093bf5dccf036c89232555
Claudinei Bido <bido.c@live.com>
2020-02-22 10:35:41 +0100

    refactored function

--- html/projects/ageInDays/static/js/script.js
+++ html/projects/ageInDays/static/js/script.js
@@ -1,16 +1,20 @@
 //Challenge 1: Your Age in Days..
 
-function ageInDays(){
-    let birthYear = prompt('What year were you born?');
-    var resultAge = (2019 - birthYear) * 365;
-    console.log(resultAge);
-    let h1 = document.createElement('h1');
-    let textAnswer = document.createTextNode('Your are ' + resultAge + ' days old!');
-    h1.setAttribute('id', 'ageInDays');
-    h1.appendChild(textAnswer);
-    document.getElementById('flex-box-result').appendChild(h1);
-}
+const ageInDays = () => {
+  let birthYear = prompt("What year were you born?");
 
-function reset(){
-    document.getElementById('ageInDays').remove();
-}
+  const date = new Date();
+  const currentYear = date.getFullYear();
+
+  const daysOld = (currentYear - birthYear) * 365;
+
+  let h1 = document.createElement("h1");
+  let textAnswer = document.createTextNode(`Your are ${daysOld} days old!`);
+  h1.setAttribute("id", "ageInDays");
+  h1.appendChild(textAnswer);
+  document.getElementById("flex-box-result").appendChild(h1);
+};
+
+const reset = () => {
+  document.getElementById("ageInDays").remove();
+};
