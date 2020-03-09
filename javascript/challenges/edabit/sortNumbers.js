45c57103f8ec7e099fd539164668cba67b17b452
Claudinei Bido <bido.c@live.com>
2020-03-09 09:16:46 +0100

    daily exercises

---
+++ javascript/challenges/edabit/sortNumbers.js
@@ -0,0 +1,22 @@
+/*
+Create a function that takes an array of numbers and returns a new array, sorted in ascending order (smallest to biggest).
+
+Sort numbers array in ascending order.
+If the function's argument is null, an empty array, or undefined; return an empty array.
+Return a new array of sorted numbers. 
+
+Don't use sort method.
+
+*/
+
+sortNumsAscending = (arr) => {
+  console.log(arr);
+};
+
+sortNumsAscending([1, 2, 10, 50, 5]); // [1, 2, 5, 10, 50]
+
+sortNumsAscending([80, 29, 4, -95, -24, 85]); // [-95, -24, 4, 29, 80, 85]
+
+// sortNumsAscending(null); // []
+
+// sortNumsAscending([]); // []
