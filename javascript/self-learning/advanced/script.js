544185a581a03bbb9b7e774b3e14fb3fc32c5bf9
Claudinei Bido <bido.c@live.com>
2020-04-05 21:15:39 +0200

    object.create

--- javascript/self-learning/advanced/script.js
+++ javascript/self-learning/advanced/script.js
@@ -1,15 +1,25 @@
-// Function constuctor
-class Person {
+// class
+/*class Person {
   constructor(name, birthYear, job) {
     this.name = name;
     this.birthYear = birthYear;
     this.job = job;
-    this.calculateAge = function () {
-      console.log(2016 - this.birthYear);
-    };
   }
+  calculateAge() {
+    console.log(2016 - this.birthYear);
+  }
+  lastName = "Smith";
 }
 
 const jahn = new Person("Jahn", 1988, "Developer");
 
 jahn.calculateAge();
+
+const jane = new Person("Jane", 1985, "Designer");
+const mark = new Person("Mark", 1949, "Retired");
+
+mark.calculateAge();
+console.log(mark.lastName);
+*/
+
+// Object.create
